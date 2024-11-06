8b240132c138646208fdc309f31270e0b7ae7177
LUKE SHEESLEY <Luke.sheesley@uvm.edu>
2024-11-06 12:48:09 -0500

    comment

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log
+function sendIdea() {
+    const email = document.getElementById("email").value;
+    const date = document.getElementById("date").value;
+    const comments = document.getElementById("comments").value;
+
+    const genres = [];
+        if (document.getElementById("rock").checked) genres.push("Rock");
+        else if (document.getElementById("jazz").checked) genres.push("Jazz");
+        else if (document.getElementById("indie").checked) genres.push("Indie");
+        else if (document.getElementById("punk").checked) genres.push("Punk");
+
+    const instrument = [];
+        if (document.getElementById("guitar").checked) instrument.push = "Guitar";
+        else if (document.getElementById("drums").checked) instrument.push = "Drums";
+        else if (document.getElementById("bass").checked) instrument.push = "Bass";
+        else if (document.getElementById("triangle").checked) instrument.push = "Triangle";
+        
+
+    // Display the output in the storage section
+    document.getElementById("output").innerHTML = output;
+
+
+event.preventDefault()
+
+}
